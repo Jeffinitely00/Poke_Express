4c00c0f9375a1218f00855763e66c19167ecc383
Jeff <jeff_lim408@icloud.com>
2023-04-29 14:37:19 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000
-const pokemon = require('./models/pokemon')
+const PORT = process.env.PORT || 3000;
+const pokemon = require("./models/pokemon");
 
-const reactViewsEngine = require('jsx-view-engine').createEngine();
-app.engine('jsx', reactViewsEngine)
+const reactViewsEngine = require("jsx-view-engine").createEngine();
+app.engine("jsx", reactViewsEngine);
 app.set("view engine", "jsx");
 app.set("views", "./views");
 
 app.use((req, res, next) => {
-    next();
-  });
+  next();
+});
 
 // I.N.D.U.C.E.S
 // =============
 // Index
 
 app.get("/", (req, res) => {
-    res.send('Welcome to the Pokemon App!')
-})
+  res.send("Welcome to the Pokemon App!");
+});
 
 app.get("/pokemon", (req, res) => {
-    res.render('index', { pokemon });
-  });
+  res.render("index", { pokemon });
+});
+
+app.get("/pokemon/:id", (req, res) => {
+    res.send(req.params.id);
+})
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
+  console.log(`Listening on port: ${PORT}`);
+});
