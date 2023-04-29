6d767165cd4577e28664eb2073a6df35e4cf0b4b
Jeff <jeff_lim408@icloud.com>
2023-04-29 14:34:22 -0500

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -3,28 +3,25 @@ const app = express();
 const PORT = process.env.PORT || 3000
 const pokemon = require('./models/pokemon')
 
-
 const reactViewsEngine = require('jsx-view-engine').createEngine();
 app.engine('jsx', reactViewsEngine)
 app.set("view engine", "jsx");
 app.set("views", "./views");
 
-app.use(express.urlencoded({ extended: false }));
-
 app.use((req, res, next) => {
     next();
   });
 
-
 // I.N.D.U.C.E.S
 // =============
 // Index
+
 app.get("/", (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get("/pokemon", (req, res) => {
-    res.render('index');
+    res.render('index', { pokemon });
   });
 
 app.listen(PORT, () => {
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,19 +1,30 @@
 const React = require("react");
 const myStyle = {
-    color: '#ffffff',
-    backgroundColor: '#000000',
-    };
-
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
 
 class Index extends React.Component {
-    render() {
-        const { pokemon } = this.props;
-        return (
-            <div style={myStyle}>
-                <h1>See All The Pokemon!</h1>
-            </div>
-        )
-    }
+  render() {
+    const pokemon = this.props.pokemon;
+    return (
+      <div>
+        <h1 style={myStyle}>See All The Pokemon!</h1>
+
+        <ul>
+          {pokemon.map((p, i) => {
+            return (
+              <li>
+                <a href={`/pokemon/${i}`}>
+                  {p.name.charAt(0).toUpperCase() + p.name.slice(1)}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 module.exports = Index;
